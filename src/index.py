1f70cda879730fba8a490263d03b59c3e40284de
Joel Alcantara <joel.alcantara@ciencias.unam.mx>
2022-10-18 17:31:14 -0500

    Se incorpora primer versión del guardado de registros

--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,12 +9,11 @@ def home():
 
 @app.route('/about')
 def about():
-    write()
     return render_template('about.html')
 
-def write():
+def write(registro):
     saludo = open("archivo.txt", "a")
-    saludo.write("Agregue otra linea")
+    saludo.write(registro)
     saludo.write("\n")
     saludo.close()
 
@@ -22,8 +21,17 @@ def read():
     archivo = open("archivo.txt")
     data = []
     for linea in archivo:
-        data.append(linea + "<br/>")
+        data.append(linea.split("#"))
     return data
+
+@app.route("/forward/", methods=["POST"])
+def move_forward():
+    nombre = request.form['nombre']
+    tipoTrabajo = request.form['tipoTrabajo']
+    fechaRetoque = request.form['fechaRetoque']
+    if(len(nombre)>1 and len(tipoTrabajo) > 1 and len(fechaRetoque) > 1):
+        write(nombre + "#" + tipoTrabajo + "#" + fechaRetoque)
+    return home()
 
 if __name__ == '__main__':
     app.run(debug=True)
