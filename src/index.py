6fce829b92c1a062a5135190b857f3bc0f5fc1ef
Joel Alcantara <joel.alcantara@ciencias.unam.mx>
2022-10-18 16:16:35 -0500

    agregando desarrollo de archivos

--- src/index.py
+++ src/index.py
@@ -4,11 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    dataFile = read()
+    return render_template('home.html', data = dataFile)
 
 @app.route('/about')
 def about():
+    write()
     return render_template('about.html')
+
+def write():
+    saludo = open("archivo.txt", "a")
+    saludo.write("Agregue otra linea")
+    saludo.write("\n")
+    saludo.close()
+
+def read():
+    archivo = open("archivo.txt")
+    data = []
+    for linea in archivo:
+        data.append(linea + "<br/>")
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
